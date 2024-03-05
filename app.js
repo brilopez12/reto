a0cd870ac44480aa8f372f53e2b06111eeb1ac9c
Brigith López <>
2024-03-05 13:02:42 -0500

    Formulario listo con js

--- app.js
+++ app.js
@@ -1,5 +1,41 @@
 function convertir(){
   let nombre =document.getElementById("nombre").value;
-  let nombre_mayus = nombre.toOperCase;
+  let nombre_mayus = nombre.toUpperCase();
+  alert(nombre_mayus);
+}
 
+function actboton(){
+  let boton = document.getElementById("boton");
+  boton.disabled=false;
+}
+
+function registro(){
+  let resgistro= document.getElementById("radio1");
+  let div_registro= document.getElementById("si_registro");
+  if(resgistro.checked){
+    div_registro.innerHTML=`
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+      <label class="form-check-label" for="flexCheckDefault">Modalidad 1</label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+      <label class="form-check-label" for="flexCheckChecked">Modalidad 2</label>
+   </div>
+    `;
+  }else{
+    div_registro.innerHTML=``;
+  }
+}
+
+function validar(){
+  let pass1 = document.getElementById(`password`);
+  let pass2 = document.getElementById(`password1`);
+  if(pass1.value != pass2.value){
+    pass1.style.borderColor=`red`;
+    pass2.style.borderColor=`red`;
+  }else{
+    pass1.style.borderColor=`green`;
+    pass2.style.borderColor=`green`;
+  }
 }
